f8dd4351456b46a4de260c5bc663a8ba9b802933
Marcel Korver <demarcellus@gmail.com>
2024-12-22 16:35:35 +0100

    day16p2

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -1,5 +1,6 @@
 #include "day16.hpp"
 		
+#include <iomanip>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -9,20 +10,13 @@
 #include <queue>
 #include <unordered_set>
 
+#include <algorithm>
 
 struct Comparator
 {
 	bool operator()(const std::pair<Position, int>& item_1, const std::pair<Position, int>& item_2) const 
 	{
 		return item_1.second > item_2.second;   // second = movement cost up and including point
-	}
-};
-
-struct Hasher
-{
-	size_t operator()(const Position& pos) const
-	{
-		return (1000000 * pos.x) + (1000 * pos.y) + pos.dir;
 	}
 };
 
@@ -52,8 +46,11 @@ bool Day16::run()
 	std::cout << "\n";
 	std::cout << "OUTPUT DATA:\n";
 	
-	long result = solveMaze(map);
-	std::cout << ">>> GPS COORDINATES SUM: [" << result << "]\n";
+	int result = solveMaze(map);
+	std::cout << ">>> SHORTEST PATH: [" << result << "]\n";
+					
+	int result2 = countTilesAlongBestPaths(map);
+	std::cout << ">>> TILES ALONG BEST PATHS: [" << result2 << "]\n";					
 					
 	return true;
 }
@@ -67,19 +64,68 @@ bool Day16::test()
 	if(ok) ok = solveMaze_test();
 	if(ok) ok = solveMaze_test2();
 	
+	if(ok) ok = countTilesAlongBestPaths_test();
+	if(ok) ok = countTilesAlongBestPaths_test2();
+	
 	return ok;
 }
 
 /**
  * CODE
  */
-long Day16::solveMaze(std::vector<std::vector<char>> map)
+int Day16::solveMaze(const std::vector<std::vector<char>> map)
 {
-	Position start = findStart(&map);
-	Position end = findEnd(&map);
+	auto [best_path_score, end_dir] = findBestPath(map);
 	
-	//std::cout << "Start at [" << start.x << "," << start.y << "]\n";
-	//std::cout << "End at [" << end.x << "," << end.y << "]\n";
+	return best_path_score;
+}
+
+int Day16::countTilesAlongBestPaths(const std::vector<std::vector<char>> map)
+{
+	auto [best_path_score, end_dir] = findBestPath(map);
+	
+	auto best_score_map = findBestScores(map, best_path_score, end_dir);
+	auto positions = walkBestPaths(map, best_score_map);
+	
+	return countUniquePositions(positions);
+}
+
+Position Day16::findStart(const std::vector<std::vector<char>>& map)
+{
+	for(int y = 0; y < (int)map.size(); y++)
+	{
+		for(int x = 0; x < (int)map[y].size(); x++)
+		{
+			if(map[y][x] == 'S')
+			{		
+				return {x, y, 1};
+			}
+		}
+	}
+	
+	return {0,0,0};
+}
+
+Position Day16::findEnd(const std::vector<std::vector<char>>& map)
+{
+	for(int y = 0; y < (int)map.size(); y++)
+	{
+		for(int x = 0; x < (int)map[y].size(); x++)
+		{
+			if(map[y][x] == 'E')
+			{		
+				return {x, y, 1};
+			}
+		}
+	}
+	
+	return {0,0,0};
+}
+
+std::pair<int,int> Day16::findBestPath(const std::vector<std::vector<char>>& map)
+{
+	Position start = findStart(map);
+	Position end = findEnd(map);
 	
 	// use a priority queue to find the shortest path
 	// this will sort itself so we will solve for the most
@@ -95,13 +141,10 @@ long Day16::solveMaze(std::vector<std::vector<char>> map)
 		// save topmost entry
 		const auto [current_position, current_score] = positions_score_queue.top();
 		positions_score_queue.pop();
-	
-		//std::cout << "Evaluating [" << current_position.x << "," << current_position.y << "][" << current_score << "]\n";
 		
 		// already visited? if not, save
 		if(positions_visited.count(current_position))
 		{
-			//std::cout << "Already visited\n";
 			continue;
 		}
 		positions_visited.insert(current_position);
@@ -109,75 +152,209 @@ long Day16::solveMaze(std::vector<std::vector<char>> map)
 		// are we at the end?
 		if(current_position.equals(end))
 		{
-			//std::cout << "Done! [" << current_score << "]\n";
-			return current_score;
+			return {current_score, current_position.dir};
 		}
 		
 		// move to positions around
-		std::vector<Position> dirs = { {0, -1, 0}, {1, 0, 1}, {0, 1, 2}, {-1, 0, 3} };
+		const std::vector<Position> dirs = { {0, -1, 0}, {1, 0, 1}, {0, 1, 2}, {-1, 0, 3} };
 		for(auto dir : dirs)
 		{
 			Position next_position = { current_position.x + dir.x, current_position.y + dir.y, dir.dir };
 			
-			//std::cout << "Trying [" << next_position.x << "," << next_position.y << "," << next_position.dir << "]\n";
-			
 			if(map.at(next_position.y).at(next_position.x) == '#')
 			{
 				// we've hit a wall
-				//std::cout << "Hit a wall\n";
 				continue;
 			}
 			else
 			{
 				// we're in an empty space
-				if(current_position.dir == next_position.dir)
+				if(next_position.moveStraight(current_position))
 				{
-					//std::cout << "Straight\n";
 					positions_score_queue.push({next_position, current_score + 1});		// save next point to check later
 				}
-				else
+				else if(next_position.moveLeftRight(current_position))
 				{
-					//std::cout << "Corner\n";
 					positions_score_queue.push({next_position, current_score + 1001});	// save next point to check later
 				}
+				else if(next_position.moveBackwards(current_position))
+				{
+					positions_score_queue.push({next_position, current_score + 2001});	// save next point to check later
+				}
 			}
+		}		
+	}
+	
+	return {-1,-1};
+}
+
+std::vector<std::vector<std::vector<int>>> Day16::findBestScores(const std::vector<std::vector<char>>& map, const int best_path_score, const int end_dir)
+{
+	Position end = findEnd(map);
+	end.dir = end_dir;
+		
+	// now we go backwards
+	std::priority_queue<std::pair<Position, int>, std::vector<std::pair<Position, int>>, Comparator> positions_score_queue;
+	std::unordered_set<Position, Hasher> positions_visited;
+	
+	// output
+	std::vector<std::vector<std::vector<int>>> best_score_map(map.size(), std::vector<std::vector<int>>(map[0].size(), {-1, -1, -1, -1}));
+	
+	positions_score_queue.push({end, 0});
+	
+	// continue until goal reached or queue empty
+	while(!positions_score_queue.empty())
+	{
+		// save topmost entry
+		const auto [current_position, current_score] = positions_score_queue.top();
+		positions_score_queue.pop();
+		
+		// already visited? if not, save
+		if(positions_visited.count(current_position))
+		{
+			continue;
+		}
+		positions_visited.insert(current_position);
+		
+		// save score
+		best_score_map[current_position.y][current_position.x][current_position.dir] = current_score;
+		
+		// move to next point with current dir
+		const std::vector<Position> dirs = { {0, -1, 0}, {1, 0, 1}, {0, 1, 2}, {-1, 0, 3} };
+		const Position dir = dirs[current_position.dir];
+		Position next_position = { current_position.x - dir.x, current_position.y - dir.y, current_position.dir };	// reverse because we start at the end
+		
+		if(map.at(next_position.y).at(next_position.x) == '#')
+		{
+			// we've hit a wall
+			continue;
 		}
 		
+		// move to directions around		
+		for(int try_dir = 0; try_dir < 4; try_dir++)
+		{
+			next_position.dir = try_dir;
+			
+			if(next_position.moveStraight(current_position))
+			{
+				positions_score_queue.push({next_position, current_score + 1});		// save next point to check later
+			}
+			else if(next_position.moveLeftRight(current_position))
+			{
+				positions_score_queue.push({next_position, current_score + 1001});	// save next point to check later
+			}
+			else if(next_position.moveBackwards(current_position))
+			{
+				positions_score_queue.push({next_position, current_score + 2001});	// save next point to check later
+			}
+		}		
 	}
 	
-	return -1;
+	return best_score_map;
 }
 
-Position Day16::findStart(std::vector<std::vector<char>> * map)
+std::unordered_set<Position, Hasher> Day16::walkBestPaths(const std::vector<std::vector<char>>& map, std::vector<std::vector<std::vector<int>>>& best_score_map)
 {
-	for(int y = 0; y < (int)map->size(); y++)
+	Position start = findStart(map);
+	Position end = findEnd(map);
+		
+	// forwards following the best path
+	std::priority_queue<std::pair<Position, int>, std::vector<std::pair<Position, int>>, Comparator> positions_score_queue;
+	std::unordered_set<Position, Hasher> positions_visited;
+		
+	positions_score_queue.push({start, best_score_map[start.y][start.x][start.dir]});
+	
+	// continue until goal reached or queue empty
+	while(!positions_score_queue.empty())
 	{
-		for(int x = 0; x < (int)map->at(y).size(); x++)
+		// save topmost entry
+		const auto [current_position, current_score] = positions_score_queue.top();
+		positions_score_queue.pop();
+		
+		// already visited? if not, save
+		if(positions_visited.count(current_position))
 		{
-			if(map->at(y).at(x) == 'S')
-			{		
-				return {x, y, 1};
+			continue;
+		}
+		positions_visited.insert(current_position);
+		
+		// are we at the end?
+		if(current_position.equals(end))
+		{
+			continue; // give other paths a chance
+		}
+		
+		// move to positions around
+		const std::vector<Position> dirs = { {0, -1, 0}, {1, 0, 1}, {0, 1, 2}, {-1, 0, 3} };
+		for(auto dir : dirs)
+		{
+			Position next_position = { current_position.x + dir.x, current_position.y + dir.y, dir.dir };
+			
+			if(map.at(next_position.y).at(next_position.x) == '#')
+			{
+				// we've hit a wall
+				continue;
 			}
+			else
+			{
+				int best_score = best_score_map[next_position.y][next_position.x][next_position.dir];
+				
+				if(next_position.moveStraight(current_position) && best_score == current_score - 1)				// check score - only best path
+				{
+					positions_score_queue.push({next_position, current_score - 1});								// save next point to check later
+				}
+				else if(next_position.moveLeftRight(current_position) && best_score == current_score - 1001)    // check score - only best path
+				{
+					positions_score_queue.push({next_position, current_score - 1001});							// save next point to check later
+				}
+				else if(next_position.moveBackwards(current_position) && best_score == current_score - 2001)    // check score - only best path
+				{
+					positions_score_queue.push({next_position, current_score - 2001});							// save next point to check later
+				}
+			}
+		}		
+	}
+	
+	// print map with positions
+	//printMap(map, positions_visited);
+	
+	return positions_visited;
+}
+
+int Day16::countUniquePositions(const std::unordered_set<Position, Hasher>& list)
+{
+	std::vector<Position> poss;
+	
+	for(auto& p : list)
+	{
+		Position pos = p;
+		pos.dir = 0;
+		if(std::find(poss.begin(), poss.end(), pos) == poss.end())
+		{
+			poss.push_back(pos);
 		}
 	}
 	
-	return {0,0,0};
+	return (int)poss.size();
 }
 
-Position Day16::findEnd(std::vector<std::vector<char>> * map)
+void Day16::printMap(const std::vector<std::vector<char>>& map, const std::unordered_set<Position, Hasher>& list)
 {
-	for(int y = 0; y < (int)map->size(); y++)
+	std::vector<std::vector<char>> map_2 = map;
+	
+	for(auto& p : list)
 	{
-		for(int x = 0; x < (int)map->at(y).size(); x++)
-		{
-			if(map->at(y).at(x) == 'E')
-			{		
-				return {x, y, 1};
-			}
-		}
+		map_2[p.y][p.x] = 'O';
 	}
 	
-	return {0,0,0};
+	for(int y = 0; y < (int)map_2.size(); y++)
+	{
+		for(int x = 0; x < (int)map_2[y].size(); x++)
+		{
+			std::cout << map_2[y][x];
+		}
+		std::cout << "\n";
+	}
 }
 
 /**
@@ -268,6 +445,74 @@ bool Day16::solveMaze_test2()
 	long result = solveMaze(map);
 	
 	if(result == 11048)
+	{
+		std::cout << "Test [" << __func__ << "] PASSED\n";
+		return true;
+	}
+	
+	std::cout << "Test [" << __func__ << "] FAILED\n";
+
+	return false;
+}
+
+bool Day16::countTilesAlongBestPaths_test()
+{
+	std::vector<std::vector<char>> map = { {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+										   {'#','.','.','.','.','.','.','.','#','.','.','.','.','E','#'},
+										   {'#','.','#','.','#','#','#','.','#','.','#','#','#','.','#'},
+										   {'#','.','.','.','.','.','#','.','#','.','.','.','#','.','#'},
+										   {'#','.','#','#','#','.','#','#','#','#','#','.','#','.','#'},
+										   {'#','.','#','.','#','.','.','.','.','.','.','.','#','.','#'},
+										   {'#','.','#','.','#','#','#','#','#','.','#','#','#','.','#'},
+										   {'#','.','.','.','.','.','.','.','.','.','.','.','#','.','#'},
+										   {'#','#','#','.','#','.','#','#','#','#','#','.','#','.','#'},
+										   {'#','.','.','.','#','.','.','.','.','.','#','.','#','.','#'},
+										   {'#','.','#','.','#','.','#','#','#','.','#','.','#','.','#'},
+										   {'#','.','.','.','.','.','#','.','.','.','#','.','#','.','#'},
+										   {'#','.','#','#','#','.','#','.','#','.','#','.','#','.','#'},
+										   {'#','S','.','.','#','.','.','.','.','.','#','.','.','.','#'},
+										   {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'} };
+	
+	std::cout << "\n";
+	
+	long result = countTilesAlongBestPaths(map);
+	
+	if(result == 45)
+	{
+		std::cout << "Test [" << __func__ << "] PASSED\n";
+		return true;
+	}
+	
+	std::cout << "Test [" << __func__ << "] FAILED\n";
+
+	return false;
+}
+
+bool Day16::countTilesAlongBestPaths_test2()
+{
+	std::vector<std::vector<char>> map = { {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+		                                   {'#','.','.','.','#','.','.','.','#','.','.','.','#','.','.','E','#'},
+		                                   {'#','.','#','.','#','.','#','.','#','.','#','.','#','.','#','.','#'},
+		                                   {'#','.','#','.','#','.','#','.','.','.','#','.','.','.','#','.','#'},
+		                                   {'#','.','#','.','#','.','#','.','#','#','#','.','#','.','#','.','#'},
+		                                   {'#','.','.','.','#','.','#','.','#','.','.','.','.','.','#','.','#'},
+		                                   {'#','.','#','.','#','.','#','.','#','.','#','#','#','#','#','.','#'},
+		                                   {'#','.','#','.','.','.','#','.','#','.','#','.','.','.','.','.','#'},
+		                                   {'#','.','#','.','#','#','#','#','#','.','#','.','#','#','#','.','#'},
+		                                   {'#','.','#','.','#','.','.','.','.','.','.','.','#','.','.','.','#'},
+		                                   {'#','.','#','.','#','#','#','.','#','#','#','#','#','.','#','#','#'},
+		                                   {'#','.','#','.','#','.','.','.','#','.','.','.','.','.','#','.','#'},
+		                                   {'#','.','#','.','#','.','#','#','#','#','#','.','#','#','#','.','#'},
+		                                   {'#','.','#','.','#','.','.','.','.','.','.','.','.','.','#','.','#'},
+		                                   {'#','.','#','.','#','.','#','#','#','#','#','#','#','#','#','.','#'},
+		                                   {'#','S','#','.','.','.','.','.','.','.','.','.','.','.','.','.','#'},
+		                                   {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'} };
+	
+	std::cout << "\n";
+	
+	long result = countTilesAlongBestPaths(map);
+	
+	if(result == 64)
 	{
 		std::cout << "Test [" << __func__ << "] PASSED\n";
 		return true;
--- day16/day16.hpp
+++ day16/day16.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <cmath>
+#include <unordered_set>
 
 struct Position
 {
@@ -13,6 +15,21 @@ struct Position
 		return (x == pos.x && y == pos.y);
 	}
 	
+	bool moveStraight(const Position& pos) const
+	{
+		return (dir == pos.dir);
+	}
+	
+	bool moveBackwards(const Position& pos) const
+	{
+		return (std::abs(dir - pos.dir) == 2);
+	}
+	
+	bool moveLeftRight(const Position& pos) const
+	{
+		return (std::abs(dir - pos.dir) == 1 || std::abs(dir - pos.dir) == 3);
+	}
+	
 	bool operator == (const Position& pos) const
 	{
 		return (x == pos.x && y == pos.y && dir == pos.dir);
@@ -21,6 +38,14 @@ struct Position
 	bool operator < (const Position& pos) const
 	{
 		return (x < pos.x || (x == pos.x && y < pos.y) || (x == pos.x && y == pos.y && dir < pos.dir));
+	}
+};
+
+struct Hasher
+{
+	size_t operator()(const Position& pos) const
+	{
+		return (1000000 * pos.x) + (1000 * pos.y) + pos.dir;
 	}
 };
 
@@ -39,12 +64,25 @@ class Day16
 	
 		bool readInput(std::vector<std::vector<char>> * map);
 
-		long solveMaze(std::vector<std::vector<char>> map);
+		int solveMaze(const std::vector<std::vector<char>> map);
+		int countTilesAlongBestPaths(const std::vector<std::vector<char>> map);
 
-		Position findStart(std::vector<std::vector<char>> * map);
-		Position findEnd(std::vector<std::vector<char>> * map); 
+		Position findStart(const std::vector<std::vector<char>>& map);
+		Position findEnd(const std::vector<std::vector<char>>& map); 
+
+		std::pair<int,int> findBestPath(const std::vector<std::vector<char>>& map);
+		std::vector<std::vector<std::vector<int>>> findBestScores(const std::vector<std::vector<char>>& map, const int best_path_score, const int end_dir);
+		std::unordered_set<Position, Hasher> walkBestPaths(const std::vector<std::vector<char>>& map, std::vector<std::vector<std::vector<int>>>& best_score_map);
+		int countUniquePositions(const std::unordered_set<Position, Hasher>& list);
+	
+	
+		void printMap(const std::vector<std::vector<char>>& map, const std::unordered_set<Position, Hasher>& list);
+
 
 		bool solveMaze_test();
 		bool solveMaze_test2();
+		
+		bool countTilesAlongBestPaths_test();
+		bool countTilesAlongBestPaths_test2();
 		
 };
